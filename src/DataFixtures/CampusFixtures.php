23d7f7d27c41ff4c1afcc1045701b785fbfef910
Abou78 <abou.diallo2021@campus-eni.fr>
2022-01-28 10:39:56 +0100

    ajout fixtures

---
+++ src/DataFixtures/CampusFixtures.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Campus;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CampusFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+    }
+}
