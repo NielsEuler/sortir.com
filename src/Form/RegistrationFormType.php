2b0aebe1957d21e483320d0af40b5ae8f7dfe860
TomC-ENI <thomas.chaffardon2021@campus-eni.fr>
2022-01-28 16:28:13 +0100

    MAJ page inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,8 +5,11 @@ namespace App\Form;
 use App\Entity\Participant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
@@ -17,27 +20,32 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('pseudo')
-            ->add('password', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Saisir un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-
-        ;
+            ->add('email', EmailType::class,
+                array('attr' => array('placeholder' => 'Votre email'),
+                'constraints' => array(
+                    new NotBlank(array("message" => "Merci d'inscrire un email valide."))
+                    )
+                )
+            )
+            ->add('nom', TextType::class,
+                array('attr' => array('placeholder' => 'Votre nom'),
+                    'constraints' => array(
+                        new NotBlank(array("message" => "Merci d'ajouter votre nom"))
+                    )
+                ))
+            ->add('password', PasswordType::class,
+                array('attr' => array('placeholder' => 'Votre mot de passe'),
+                    'constraints' => array(
+                        new NotBlank(array("message" => 'Saisir un mot de passe')),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096
+                        ])
+                    )
+                )
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
