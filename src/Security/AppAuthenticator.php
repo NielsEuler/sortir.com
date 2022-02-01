4a7f9deaa61a1a36523888d6f6b2741ee57234f0
TomC-ENI <thomas.chaffardon2021@campus-eni.fr>
2022-02-01 16:39:05 +0100

    maj menu (lien) css

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('main_home'));
+        return new RedirectResponse($this->urlGenerator->generate('main_afficherSortie'));
     }
 
     protected function getLoginUrl(Request $request): string
