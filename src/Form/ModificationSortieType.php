cfadf03f35d3e9334f91577393aa543460124bc8
niels <niels.euler2021@campus-eni.fr>
2022-02-03 11:21:26 +0100

    Commit Entities

--- src/Form/ModificationSortieType.php
+++ src/Form/ModificationSortieType.php
@@ -4,9 +4,12 @@
 namespace App\Form;
 
 use App\Entity\Sortie;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,9 +22,25 @@ class ModificationSortieType extends AbstractType
             ->add('nom', TextType::class, [
                 'label' => 'Nom'
             ])
-            ->add('dateHeureDebut', DateType::class, [
+            ->add('dateHeureDebut', DateTimeType::class, [
                 'label' => 'dateHeureDebut'
             ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'label' => 'dateLimiteInscription'
+            ])
+
+            ->add('nbInscriptionsMax', IntegerType::class, [
+                'label' => 'nbInscriptionsMax'
+            ])
+
+            ->add('duree', IntegerType::class, [
+                'label' => 'duree'
+            ])
+            ->add('siteOrganisateur', TextType::class, [
+                'label' => 'siteOrganisateur'
+            ])
+
+
 
         ;
     }
